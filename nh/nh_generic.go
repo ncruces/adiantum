b6059c569cae0e49e810023b54cd803d77367d40
Nuno Cruces <ncruces@users.noreply.github.com>
2024-05-09 10:37:35 +0100

    nh: improved fallback

--- nh/nh_generic.go
+++ nh/nh_generic.go
@@ -12,12 +12,23 @@ func sum(out *[32]byte, m []byte, key []byte) {
 	}
 
 	var sums [4]uint64
-	for len(m) > 0 {
-		copy(k[:], k[4:])
-		for i := 12; i < 16; i++ {
-			k[i] = binary.LittleEndian.Uint32(key[:4])
-			key = key[4:]
-		}
+	for len(m) >= 16 && len(key) >= 16 {
+		k[0] = k[4]
+		k[1] = k[5]
+		k[2] = k[6]
+		k[3] = k[7]
+		k[4] = k[8]
+		k[5] = k[9]
+		k[6] = k[10]
+		k[7] = k[11]
+		k[8] = k[12]
+		k[9] = k[13]
+		k[10] = k[14]
+		k[11] = k[15]
+		k[12] = binary.LittleEndian.Uint32(key[0:4])
+		k[13] = binary.LittleEndian.Uint32(key[4:8])
+		k[14] = binary.LittleEndian.Uint32(key[8:12])
+		k[15] = binary.LittleEndian.Uint32(key[12:16])
 
 		m0 := binary.LittleEndian.Uint32(m[0:4])
 		m1 := binary.LittleEndian.Uint32(m[4:8])
@@ -32,6 +43,8 @@ func sum(out *[32]byte, m []byte, key []byte) {
 		sums[1] += uint64(m1+k[5]) * uint64(m3+k[7])
 		sums[2] += uint64(m1+k[9]) * uint64(m3+k[11])
 		sums[3] += uint64(m1+k[13]) * uint64(m3+k[15])
+
+		key = key[16:]
 		m = m[16:]
 	}
 
